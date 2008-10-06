f91b733aa8828ef22a9e05dc1474635afd5dd298
Darren Case <darren@darren-cases-macbook.local>
2008-10-06 11:55:57 -0400

    changed inquiry recipient to info@commandc.com

--- app/models/inquiry_mailer.rb
+++ app/models/inquiry_mailer.rb
@@ -3,8 +3,8 @@ class InquiryMailer < ActionMailer::Base
 
   def inquiry(inquiry)
     subject    'new inquiry from commandc.com'
-    recipients 'case_darren@yahoo.com'
-    from       'info@commandc.com'
+    recipients 'info@commandc.com'
+    from       'COMMAND C inquiry <inquiries@commandc.com>'
     content_type 'text/html'
     sent_on    Time.now
     
