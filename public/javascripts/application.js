ec8167b16a4c07cac388e6d2711aaa194a3642dc
Darren Case <darren@darren-cases-macbook.local>
2008-10-14 13:55:49 -0400

    tweaks as outlined in 10.13.08.doc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function set_current_category(category_id) {
-	new Ajax.Request('/portfolio_menu_items/set_current', { 
+function set_current_category(category_id, redirect) {
+	var options = { 
 		parameters : {
 			id : category_id,
 			authenticity_token : AUTH_TOKEN
 			}
-		});
+		};
+	if(typeof(redirect) != "undefined") {
+		Object.extend(options, {
+			onComplete: function() {
+				window.location.href = redirect;
+			}
+		})
+	}
+	
+	new Ajax.Request('/portfolio_menu_items/set_current', options);
 }
