6e896d64709dbd0e9c8f6d8da358b811ad5ccbe6
Darren Case <darren@darren-cases-macbook.local>
2008-11-10 17:41:50 -0500

    edited twitter heading

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -7,7 +7,7 @@ module StaticHelper
     feed = SimpleRSS.parse open( 'http://commandc.com/blog1/feed' )
     
     output = "<div id=\"blog_posts\">"
-    output += image_tag('blog-separator.gif', :size => "111x60")
+    output += image_tag('blog-separator.gif', :size => "111x61")
     output += "<div class=\"blog-post\">>> " + link_to(truncate(feed.items[0].description, 75),{},:href => feed.items[0].link, :target => "_blank" ) + "</div>"
     rescue OpenURI::HTTPError => e
       output = e
@@ -15,7 +15,7 @@ module StaticHelper
     
     twitter = Twitter::Base.new('info@commandc.com','commandc50').timeline(:user).first
     
-    output += image_tag('twitter-separator.gif', :size => "111x60")
+    output += image_tag('twitter-separator.gif', :size => "94x61")
     output += "<div class=\"blog-post\">>> " + link_to(truncate(twitter.text, 75),{},:href => "http://twitter.com/commandc", :target => "_blank" ) + "</div>"
     output += "</div>"
     
