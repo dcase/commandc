d7056ffcdd6341123def51c2a95685d25d601d92
Darren Case <darren@darren-cases-macbook.local>
2008-11-10 06:27:38 -0500

    added copyright to layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :imagefile
-  has_many :portfolio_menu_items, :dependent => :destroy
+  has_many :portfolio_menu_items, :dependent => :destroy, :order => "position"
   has_many :categories, :through => :portfolio_menu_items
   has_many :tabs, :order => "position", :dependent => :destroy
   
