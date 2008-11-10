c4d46b7a3f0bc3c6ca18ff2bd0e75fb088e3f420
Darren Case <darren@darren-cases-macbook.local>
2008-11-10 16:07:27 -0500

    added twitter feed to footer

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -7,14 +7,17 @@ module StaticHelper
     feed = SimpleRSS.parse open( 'http://commandc.com/blog1/feed' )
     
     output = "<div id=\"blog_posts\">"
-    output += image_tag('blog-separator-1.gif', :size => "111x60")
+    output += image_tag('blog-separator.gif', :size => "111x60")
     output += "<div class=\"blog-post\">>> " + link_to(truncate(feed.items[0].description, 75),{},:href => feed.items[0].link, :target => "_blank" ) + "</div>"
-    output += image_tag('blog-separator-2.gif', :size => "74x60")
-    output += "<div class=\"blog-post\">>> " + link_to(truncate(feed.items[1].description, 75),{},:href => feed.items[1].link, :target => "_blank" ) + "</div>"
-    output += "</div>"
     rescue OpenURI::HTTPError => e
       output = e
     end
+    
+    twitter = Twitter::Base.new('info@commandc.com','commandc50').timeline(:user).first
+    
+    output += image_tag('twitter-separator.gif', :size => "111x60")
+    output += "<div class=\"blog-post\">>> " + link_to(truncate(twitter.text, 75),{},:href => "http://twitter.com/commandc", :target => "_blank" ) + "</div>"
+    output += "</div>"
     
     output
     
