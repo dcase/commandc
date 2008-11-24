bf724d22481a1a9b8121bdae823164d19af76b59
Darren Case <darren@darren-cases-macbook.local>
2008-11-24 05:26:04 -0500

    make portfolio menu properly ordered 1

--- app/models/portfolio_menu_item.rb
+++ app/models/portfolio_menu_item.rb
@@ -1,5 +1,5 @@
 class PortfolioMenuItem < ActiveRecord::Base
   acts_as_list :scope => :category
-  belongs_to :project
-  belongs_to :category
+  belongs_to :project, :order => 'position'
+  belongs_to :category, :order => 'position'
 end
