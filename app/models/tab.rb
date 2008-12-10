ab7518ac9cb7841a686439cd81650aba6d975321
Darren Case <darren@darren-cases-macbook.local>
2008-12-10 08:53:04 -0500

    removed validation of content for tab model

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -3,6 +3,6 @@ class Tab < ActiveRecord::Base
   belongs_to :project
   acts_as_list :scope => :project
   
-  validates_presence_of :title, :content
+  validates_presence_of :title
   validates_presence_of :imagefile,  :message => ": A file must be selected"
 end
