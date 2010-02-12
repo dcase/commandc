4f387994ef950a7485ddeda214ec3704d6e81119
Darren Case <darren@darren-cases-macbook.local>
2010-02-12 12:28:39 -0500

    adding akismet spam protection

--- app/models/inquiry_mailer.rb
+++ app/models/inquiry_mailer.rb
@@ -3,7 +3,7 @@ class InquiryMailer < ActionMailer::Base
 
   def inquiry(inquiry)
     subject    proc { inquiry.is_spam ? '**SPAM** inquiry from commandc.com' : 'new inquiry from commandc.com' }
-    recipients 'info@commandc.com'
+    recipients 'case_darren@yahoo.com'
     from       'COMMAND C inquiry <inquiries@commandc.com>'
     content_type 'text/html'
     sent_on    Time.now
